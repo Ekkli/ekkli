4ce0287d5ebc98f96649f3b28698f0eb4ded049f
dibau <dibaunaumh@gmail.com>
2013-05-28 23:00:53 +0300

    fixed bug when deleting stories

--- client/map.js
+++ client/map.js
@@ -303,8 +303,10 @@ Template.map.rendered = function() {
 			});
             var links = [];
             _.forEach(stories, function(story) {
+				if (story) {
 				for (var i = 0; i < story.nextStories.length; i++) {
 					var linkedStory = story_by_id[story.nextStories[i]];
+					if (linkedStory) {
 					var link_color = story.nextStoriesLinks[i].color;
                     var link = {
                         from: story._id,
@@ -316,7 +318,9 @@ Template.map.rendered = function() {
 						color: link_color
                     };
                     links.push(link);
+					}
                 };
+				}
             });
             console.log(links);
             var x1 = function(d) { return d.x1 },
