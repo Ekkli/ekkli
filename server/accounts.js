f6be881ec029b8f8db1258743531574ebe8bdc5d
vlad <voskoboynikvlad@gmail.com>
2013-07-15 11:57:36 +0300

    login with /without google account(invite users)

--- server/accounts.js
+++ server/accounts.js
@@ -1,30 +1,60 @@
 Accounts.onCreateUser(function (options, user) {
-    var accessToken = user.services.google.accessToken,
-        result,
-        profile;
 
-    result = Meteor.http.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", {
-        params: {
-            access_token: accessToken
+    console.log(JSON.stringify(options));
+
+    user.profile = options.profile || {};
+    user.createdAt = new Date();
+
+    if (options.email)
+        user.profile.email = options.email;
+
+
+    if (user.services.facebook)
+    {
+//        get profile data from Facebook
+        result = Meteor.http.get("https://graph.facebook.com/me",{
+            params:{
+                access_token: user.services.facebook.accessToken
+            }
+        });
+//            if successfully obtained facebook profile, save it off
+        if (!result.error && result.data){
+            user.profile.facebook = result.data;
         }
-    });
+    }
 
-    console.log("result :"+result.data);
 
-    if (result.error)
-        throw result.error;
+    if (user.services.google){
+        var accessToken = user.services.google.accessToken,
+            result,
+            profile;
 
-    profile = _.pick(result.data,
-        "login",
-        "name",
-        "avatar_url",
-        "url",
-        "company",
-        "blog",
-        "location",
-        "email");
+        result = Meteor.http.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", {
+            params: {
+                access_token: accessToken
+            }
+        });
 
-    user.profile = profile;
+        console.log("result :"+result.data);
+
+        if (result.error)
+            throw result.error;
+
+        profile = _.pick(result.data,
+            "login",
+            "name",
+            "avatar_url",
+            "url",
+            "company",
+            "blog",
+            "location",
+            "email");
+
+        user.profile = profile;
+
+    }
 
     return user;
+
+
 });
