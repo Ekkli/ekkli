c6777d8ea4d7010065848854603e974052554d75
vlad <voskoboynikvlad@gmail.com>
2013-10-12 13:11:20 +0200

    config for facebook

--- server/config.js
+++ server/config.js
@@ -17,3 +17,9 @@ Accounts.loginServiceConfiguration.insert({
     consumerKey: "tNEzCcq6vy5zLaJmfpLAA",
     secret: "BwR4YrOtR0YMdt10r5rGraKvk25ZGEuiogbOx6cg5Bw"
 });
+
+Accounts.loginServiceConfiguration.insert({
+    service: "facebook",
+    clientId: "115782818510137",
+    secret: "58a1bf1057cd12606e161d4130586502"
+});
