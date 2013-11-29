10c39f55689623efa1adf5065f95d9124ca0f60c
dibau <dibaunaumh@gmail.com>
2013-11-29 06:08:33 +0200

    move startup code to deferred

--- server/server.js
+++ server/server.js
@@ -141,7 +141,10 @@ getRelatedContexts = function(contextId) {
 
 Meteor.startup(function () {
 //    process.env.MAIL_URL="smtp://postmaster%40ekkli.mailgun.org:7d0sxp--frf1@smtp.mailgun.org:587";
-    process.env.MAIL_URL="smtp://ekkliapp%40gmail.com:ekkli1234@smtp.gmail.com:465/";
+	Meteor.defer(function() {
+		console.log("Server startup done");
+		process.env.MAIL_URL="smtp://ekkliapp%40gmail.com:ekkli1234@smtp.gmail.com:465/";
+	});
 });
 
 
