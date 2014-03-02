f0394a587c6f78f29486d3e16a5f88d010d5f648
dibau <dibaunaumh@gmail.com>
2014-03-02 15:34:32 +0200

    fixes due to meteor upgrade

--- server/server.js
+++ server/server.js
@@ -140,6 +140,33 @@ getRelatedContexts = function(contextId) {
 
 
 Meteor.startup(function () {
+	// Accounts.loginServiceConfiguration.remove({
+	//     service:"github"
+	// });
+
+	Accounts.loginServiceConfiguration.insert({
+	    service:"github",
+	    clientId:"c646cadb7bc44e47c82e",
+	    secret:"c7411b5e92b8647e151feb9d757da01b82a18d78"
+	})
+
+
+	// Accounts.loginServiceConfiguration.remove({
+	//     service: "twitter"
+	// });
+	Accounts.loginServiceConfiguration.insert({
+	    service: "twitter",
+	    consumerKey: "tNEzCcq6vy5zLaJmfpLAA",
+	    secret: "BwR4YrOtR0YMdt10r5rGraKvk25ZGEuiogbOx6cg5Bw"
+	});
+
+	Accounts.loginServiceConfiguration.insert({
+	    service: "facebook",
+	    appId: "115782818510137",
+	    secret: "58a1bf1057cd12606e161d4130586502"
+	});
+
+	
 //    process.env.MAIL_URL="smtp://postmaster%40ekkli.mailgun.org:7d0sxp--frf1@smtp.mailgun.org:587";
 	Meteor.defer(function() {
 		console.log("Server startup done");
